a92f66a49e2e9fe71262130ba53550f4817a98b9
suhas <hi@suhas.one>
2022-09-09 21:53:53 -0500

    so it turns out that i have stupid

--- src/routes/api/submit/+server.ts
+++ src/routes/api/submit/+server.ts
@@ -9,7 +9,7 @@ export async function POST(e: RequestEvent){
     const postBody = {
         fields: { Prompt: data.prompt, Email: data.email }
     } 
-    console.log(process.env['AIRTABLE_API_KEY'])
+    console.log(import.meta.env.AIRTABLE_API_KEY)
     const req = await fetch('https://api.airtable.com/v0/appg9wvRrWyIpiRQN/Prompts', {
         method: 'POST',
         headers: {
